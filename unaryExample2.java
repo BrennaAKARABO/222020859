1a4c726f381352d38bb8b33ffd184a3c7e1a24e6
BrennaAKARABO <150015189+BrennaAKARABO@users.noreply.github.com>
2023-11-06 19:46:23 +0200

    Update unaryExample2.java

--- unaryExample2.java
+++ unaryExample2.java
@@ -1,5 +1,6 @@
-public class unaryExample2 {
-    public static void main(String[] args) {
+public class unaryExample2 {// class name 
+    public static void main(String[] args) {// mandatory main method
+       // initialize values
         int a=10;  
 int b=10;  
 System.out.println(a++ + ++a);//10+12=22  
