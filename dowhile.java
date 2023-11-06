1ccc68ae1479c494d9a8f1835b2416476e21a6f2
BrennaAKARABO <150015189+BrennaAKARABO@users.noreply.github.com>
2023-11-06 19:42:47 +0200

    Update dowhile.java

--- dowhile.java
+++ dowhile.java
@@ -1,8 +1,8 @@
-public class dowhile {
-    public static void main(String[] args) {
-        int i = 10;
+public class dowhile {// class name
+    public static void main(String[] args) {// mandatory main method
+        int i = 10;//initialize value
         do {
-            System.out.println(i);
+            System.out.println(i);// prints output if the condition is true
             i++;
         } while (i <= 20);
     }
