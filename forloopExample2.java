c78685e4d4683079946987ed949c93fdd4010a1b
BrennaAKARABO <150015189+BrennaAKARABO@users.noreply.github.com>
2023-11-06 19:45:24 +0200

    Update forloopExample2.java

--- forloopExample2.java
+++ forloopExample2.java
@@ -1,9 +1,9 @@
-public class forloopExample2 {
+public class forloopExample2 {// class name 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {// mandatory main method
         for (int i = 1; i <= 50; ++i) {
-            System.out.println(i);
+            System.out.println(i);// prints the output if the condition is true
         }
-        System.out.println("These are the numbers to be ranked");
+        System.out.println("These are the numbers to be ranked");//output message
     }
 }
